02e757f9b1608ad2875ff2386aeff6d84e3c3c32
Nathan Beka <nathan.beka@gmail.com>
2019-07-03 17:07:14 +0100

    Added environment variable for jupyter password

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ $# -eq 0 ]
   then
-    jupyter lab --ip=0.0.0.0 --NotebookApp.token='local-development' --allow-root --no-browser &> /dev/null &
-    code-server*/code-server --allow-http --no-auth --data-dir /data /code
+    jupyter lab --ip=0.0.0.0 --NotebookApp.password=$JUPYTER_PASS --allow-root --no-browser &> /dev/null &
+    code-server*/code-server --allow-http --user-data-dir /data /code
   else
     exec "$@"
 fi
